c40189441b48fea2b2140efea99bc138ec9aa35c
Hammad Akram <hmmadakram786@gmail.com>
2024-09-05 12:03:35 +0500

    add live backend URL

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
+export const API_BASE_URL = "https://newvisionclothingbackend.netlify.app/.netlify/functions/api/";
 
 // Check if JWT exists in localStorage
 const jwt = localStorage.getItem("jwt");
